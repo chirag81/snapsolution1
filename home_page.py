6f98fe4ca6752f2ff79c7131a7fbcfee9cc8931a
Chirag <chiragbamba81@gmail.com>
2022-06-04 13:16:44 +0530

    aq

--- home_page.py
+++ home_page.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 def app():
     st.title('snap solutions')
+
+    st.write('Step by Step Guide for doing imaga classification')
+    st.subheader('Step 1: Choose a Dataset')
+    st.text('Choose a dataset of your interest or you can also create your own '
+            'image dataset for solving your own image classification problem. '
+            'An easy place to choose a dataset is on kaggle.com')
