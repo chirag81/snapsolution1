17c1255450923fecbaeaa8cb3c0086304764dcd2
Chirag <chiragbamba81@gmail.com>
2022-06-04 13:44:16 +0530

    chira

--- home_page.py
+++ home_page.py
@@ -1,15 +1,16 @@
 import streamlit as st
-from PIL import Image
 def app():
     st.title('snap solutions')
 
     st.write('Step by Step Guide for doing imaga classification')
-    st.image("C:\python help\cnn1.png")
     st.subheader('Step 1: Choose a Dataset')
     st.text('Choose a dataset of your interest or you can also create your own ')
     st.text('image dataset for solving your own image classification problem. ')
     st.text('An easy place to choose a dataset is on kaggle.com')
     st.subheader('Step 2: import libraries for the programs')
+    st.text('you have to import reuired librabries for the programs ')
+    st.text('for example ')
+    st.text('tensorflow,numpy,keras,PIL')
     st.subheader('Step 3: Prepare Dataset for Training')
     st.text('Preparing dataset for training will involve assigning paths and')
     st.text(' creating categories(labels), resizing images.')
