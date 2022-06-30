0caee66684bfd31194563d11eed3766a4ec2b537
Chirag <chiragbamba81@gmail.com>
2022-06-30 07:10:12 +0530

    par

--- home_page.py
+++ home_page.py
@@ -1,6 +1,8 @@
 import streamlit as st
+from PIL import Image
 def app():
     st.title('snap solutions')
 
     st.subheader('welcome to the world of images')
-    st.image("C:\python help\cnn1.png")
+    image1 = Image.open(r"C:\python help\cnn1.png")
+    st.image(image1)
