6e34336079f638a0108e7258093b875df9708bac
Chirag <chiragbamba81@gmail.com>
2022-06-30 07:17:16 +0530

    djsbfiusnfo

--- home_page.py
+++ home_page.py
@@ -4,9 +4,3 @@ def app():
     st.title('snap solutions')
 
     st.subheader('welcome to the world of images')
-    if image_file is not None:
-        image = Image.open("C:\python help\cnn1.png")
-        img1 = image.resize((128, 128))
-        img1 = preprocessing.image.img_to_array(img1)
-        img1 = np.expand_dims(img1, axis=0)
-        st.image(img1)
